a5984a6b4925b1b15546a052b0884e4128abd999
Aman Murya <m.amanmaurya49@gmail.com>
2024-01-14 18:45:19 +0530

    added pagination

--- controllers/products.js
+++ controllers/products.js
@@ -30,8 +30,17 @@ const getAllProducts = async (req, res) => {
       apiData=apiData.select(selectFix);
     }
 
+    let page=Number(req.query.page)||1;
+    let limit=(req.query.limit)||3;
+    let skip=(page-1)*limit;
+
+    apiData=apiData.skip(skip).limit(limit);
+    
+    console.log(queryObject);
+
     const mydata=await apiData;
-    res.status(200).json({ mydata });
+
+    res.status(200).json({ mydata,nbHits:mydata.length });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
